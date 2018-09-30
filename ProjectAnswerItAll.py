aff9a84ab7da123f82f6e1a49fd8930fcbe085e4
adityac0620 <adityac0620@gmail.com>
2018-09-30 21:08:29 +0530

    This removes the appearance of reference tags from the page.
    This commit makes these major changes:
         1. Ability to search for custom wikipedia pages.
         2. Removes the reference tags from the wikipedia pages.

--- ProjectAnswerItAll.py
+++ ProjectAnswerItAll.py
@@ -4,7 +4,7 @@ import lxml
 from urllib.request import urlopen as uo
 
 # Declaring the url and getting the full webpage using urllib. 
-url="https://en.wikipedia.org/wiki/Apache_License"
+url=input("Enter a link to the topic's WikiPedia page : ")
 uclient=uo(url)
 page_html=uclient.read()
 uclient.close()
@@ -13,6 +13,14 @@ uclient.close()
 page_soup = soup(page_html, "html.parser")
 # Finding all paragraphs in "bodyContent" div's of the "content" div. 
 datadump=page_soup.body.find("div", {'id':'content'}).find("div",{'id':'bodyContent'}).find_all("p")
+
+#filtering out all the reference tags in the wikipedia page
+for udd in datadump:
+    m=udd.find_all("sup")
+    if m != None:
+        for i in m:
+            i.string=""
+
 # Printing the collected info.
 for dd in datadump:
     onlyTextDump=dd.get_text()
